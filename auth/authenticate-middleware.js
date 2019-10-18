580c63b81ed90756c4fcb9ac40fba78a24562dd8
noahfranco <noahfra@gmail.com>
2019-10-18 12:06:53 -0400

    API endpoint are passing successfully

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,27 @@
+const jwt = require("jsonwebtoken"); 
+const secrets = require("../secrets.js"); 
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+  const token =  req.headers.authorization; 
+
+  if(token) {
+      jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+          if(error) {
+              res.status(401).json({ message: "bad request" })
+          } else {
+              req.user = {
+                  username: decodedToken.username,
+                  department: decodedToken.department
+              }
+              next()
+          }
+      })
+  } else {
+      res.status(401).json({ you: 'shall not pass!' });
+  }
+}
